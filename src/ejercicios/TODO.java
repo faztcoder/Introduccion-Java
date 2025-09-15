0cbc9db23e7fe1b1d61304d2ed37e6782450a52b
Anthony <anthonysnlpz@gmail.com>
2025-09-15 00:09:12 -0600

    FIX al método deleteLastTODO()

---
+++ src/ejercicios/TODO.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+import java.util.List;
+
+public interface TODO {
+
+    void setAgregarTODO(List<String> TODO);
+    void getTODO();
+    void getFirst();
+    void getLastTODO();
+
+    void deleteLastTODO();
+}
--- src/ejercicios/TODOList.java
+++ src/ejercicios/TODOList.java
@@ -1,48 +1,94 @@
 package ejercicios;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class TODOList {
+public class TODOList implements TODO {
 
     public static Scanner sc = new Scanner(System.in);
-    // Tiene que cunplir el contrato de una Interfaz de List
     public List<String> listaTareas = new ArrayList<>();
-
-    public int longitud = listaTareas.size();
-    public int opcion;
+    public int opcion = 1;
 
     public void menu() {
-        do {
+        while (opcion != 0) {
+            System.out.println("\n");
             System.out.println("0. Salir");
             System.out.println("1. Agregar TODO");
             System.out.println("2. Obtener TODOS");
+            System.out.println("3. Obtener primer TODO");
+            System.out.println("4. Obtener último TODO");
+            System.out.println("5. Eliminar último TODO");
             opcion = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
 
             switch (opcion) {
-                case 0 -> System.out.println("Saliendo...");
-                case 1 -> setAgregarTODO();
-                case 2 -> getTODO();
-                default -> System.out.println("Opcion no encontrada");
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+
+                case 1:
+                    System.out.println("Agrega un TODO:");
+                    String tarea = sc.nextLine();
+                    setAgregarTODO(Collections.singletonList(tarea));
+                    break;
+
+                case 2:
+                    getTODO();
+                    break;
+
+                case 3:
+                    getFirst();
+                    break;
+
+                case 4:
+                    getLastTODO();
+                    break;
+
+                case 5:
+                    deleteLastTODO();
+                    break;
+
+                default:
+                    System.out.println("Opción no encontrada⚠️");
+                    break;
             }
-
-        } while (opcion != 0);
-
-    }
-
-    public void setAgregarTODO() {
-
-        for (int i = 0; i <= longitud; i++) {
-            System.out.println("Agrega un TODO");
-            String TODO = sc.nextLine();
-            listaTareas.add(TODO);
         }
     }
 
-    public List<String> getTODO() {
-        return listaTareas;
+    @Override
+    public void setAgregarTODO(List<String> TODO) {
+        System.out.println("\nTODO agregado 👉🏻 " + listaTareas);
+        listaTareas.addAll(TODO);
+    }
+
+    @Override
+    public void getTODO() {
+        System.out.println("Todos los TODOS 👉🏻 " + listaTareas);
+    }
+
+    @Override
+    public void getFirst() {
+        System.out.println("\nPrimer TODO agregado 👉🏻 " + listaTareas.getFirst());
+        listaTareas.getFirst();
+    }
+
+    @Override
+    public void getLastTODO() {
+        System.out.println("\nÚltimo TODO agregado 👉🏻 " + listaTareas.getLast());
+        listaTareas.getLast();
+    }
+
+    @Override
+    public void deleteLastTODO() {
+        System.out.println("\nTODO eliminado 👉🏻 " + listaTareas.removeLast());
+        if (Boolean.parseBoolean(listaTareas.getFirst())) {
+            System.out.println("Eliminando el primer TODO agregado");
+            listaTareas.clear();
+        } else {
+            listaTareas.removeLast();
+        }
     }
 
     public static void main(String[] args) {
@@ -50,3 +96,4 @@ public class TODOList {
         tdl.menu();
     }
 }
+
